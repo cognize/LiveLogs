2d08a37247a4b58debff9090902c4788e244b969
Gareth Brown <g@cognize.co.uk>
2012-11-16 11:31:58 +0000

    Initial commit to repository

---
+++ LiveLogs.ConsoleApp/MySql_README.txt
@@ -0,0 +1,22 @@
+﻿To Use in a MySql Query Monitoring Context:
+===========================================
+
+See Stack Overflow Post: http://stackoverflow.com/questions/568564/how-can-i-view-live-mysql-queries/9398566#9398566
+
+1. Edit the file located at:
+
+C:\Program Files (x86)\MySQL\MySQL Server 5.5\my.ini
+
+Add "add log=development.log" to the bottom of the file, with a comment. (Note saving this file required me to run my text editor as an admin).
+
+Use MySql workbench to open a command line, enter the password.
+
+Run the following to turn on general logging which will record all queries ran:
+
+"SET GLOBAL general_log = 'ON';"
+
+("SET GLOBAL general_log = 'OFF';" to turn off).
+
+This will produce a text file at the following location.
+
+C:\ProgramData\MySQL\MySQL Server 5.5\data\development.log
---
+++ LiveLogs.ConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+
+namespace LiveLogs.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console sizing can cause exceptions if you are using a 
+            // small monitor. Change as required.
+
+            Console.SetWindowSize(152, 58);
+            Console.BufferHeight = 1500;
+
+            string filePath = ConfigurationManager.AppSettings["MonitoredTextFilePath"];
+
+            Console.Title = string.Format("Live Logs {0}", filePath);
+
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+
+            // Move to the end of the stream so we do not read in existing
+            // log text, only watch for new text.
+
+            fileStream.Position = fileStream.Length;
+
+            StreamReader streamReader;
+
+            // Commented lines are for duplicating the log output as it's written to 
+            // allow verification via a diff that the contents are the same and all 
+            // is being output.
+
+            // var fsWrite = new FileStream(@"C:\DuplicateFile.txt", FileMode.Create);
+            // var sw = new StreamWriter(fsWrite);
+
+            int rowNum = 0;
+
+            while (true)
+            {
+                streamReader = new StreamReader(fileStream);
+
+                string line;
+                string rowStr;
+
+                while (streamReader.Peek() != -1)
+                {
+                    rowNum++;
+
+                    line = streamReader.ReadLine();
+                    rowStr = rowNum.ToString();
+
+                    string output = String.Format("{0} {1}:\t{2}", rowStr.PadLeft(6, '0'), DateTime.Now.ToLongTimeString(), line);
+
+                    Console.WriteLine(output);
+
+                    // sw.WriteLine(output);
+                }
+
+                // sw.Flush();
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
